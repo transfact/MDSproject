8802b4156b570eb03f9e3c5c27446cef891be7a2
transfact <transpaction@naver.com>
2023-10-23 20:55:59 +0900

    Tab

--- react-front/src/compo/summary/VictoryRateSummary.js
+++ react-front/src/compo/summary/VictoryRateSummary.js
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import DeckSummary from './vrs/DeckSummary';
 
 export default function VictoryRateSummary() {
+    const [season, setSeason] = useState(0);
     const DeckList = [1, 2, 3, 4, 5];
     return (
         <div>
             {DeckList.map((deck, idx) => {
-                return <DeckSummary key={idx}></DeckSummary>;
+                return (
+                    <>
+                        <DeckSummary key={idx}></DeckSummary>
+                    </>
+                );
             })}
         </div>
     );
--- react-front/src/compo/summary/vrs/DeckSummary.js
+++ react-front/src/compo/summary/vrs/DeckSummary.js
@@ -1,3 +1,45 @@
+import styled from 'styled-components';
+import ImageManager from '../../../common/ImageManger';
+import TabBtn from './decksummary/TabButton';
+
+const FlexContainer = styled.div`
+    display: flex;
+    padding: 10px;
+    background-color: white;
+    margin: 0 10px;
+    border-bottom: 1px #f7f7f9 solid;
+`;
+const FlexDiv = styled.div`
+    flex: 1;
+    line-height: 35pt;
+    padding-left: 3px;
+`;
+const SmallDiv = styled.div`
+    font-size: small;
+    color: lightgray;
+    line-height: 16pt;
+`;
+const DeckImg = styled.img`
+    border-radius: 50%;
+    width: 35px;
+    height: 35px;
+`;
+
 export default function DeckSummary() {
-    return <div>DeckSummary</div>;
+    return (
+        <FlexContainer>
+            {/* 실제는 props값을 참조하겠죠. */}
+            <TabBtn></TabBtn>
+            <DeckImg src={ImageManager.BlueEyes} />
+            <FlexDiv>푸른 눈</FlexDiv>
+            <FlexDiv>
+                <SmallDiv>선공횟수</SmallDiv>
+                <SmallDiv>Y회</SmallDiv>
+            </FlexDiv>
+            <FlexDiv>
+                <SmallDiv>XX승 XX패</SmallDiv>
+                <SmallDiv>승률 ??%</SmallDiv>
+            </FlexDiv>
+        </FlexContainer>
+    );
 }
